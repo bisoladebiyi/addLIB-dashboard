fcdfc31896c9f07b705d3caf0dc8bf1fa9b6115f
Abisola Adebiyi <abisola.ca@gmail.com>
2025-01-22 05:21:39 -0800

    create 404 UI

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import style from "./NotFound.module.scss";
+
 const NotFound = () => {
   return (
-    <div>Notfound</div>
-  )
-}
+    <div className={style.notfound}>
+      <Link to="/" className={style.notfound_logo}>
+        <figure>
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/674be8651dc4c402d52111ad/00802580-8cbc-4e40-8173-058132d383a0/addLIBFullLogo.png?format=500w"
+            alt="logo"
+          />
+        </figure>
+      </Link>
 
-export default NotFound
+      <div className={style.notfound_texts}>
+        <h1>404</h1>
+        <p>OOPS! LOOKS LIKE YOU'RE LOST</p>
+        <p>
+          The page you're looking for does not exist or may have been removed.
+        </p>
+        <Link to="/"> Go back to home</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
